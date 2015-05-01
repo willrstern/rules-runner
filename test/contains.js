d2f0ba4a4c704898f3bc2ba59e066e9c2414b2a4
Will Stern <willrstern@gmail.com>
2015-05-01 10:20:47 -0500

    1.0.11 - better testing for contains arrays

--- test/contains.js
+++ test/contains.js
@@ -2,50 +2,69 @@ var assert = require("assert");
 var Rules = require("../lib/Rules");
 
 describe("contains comparator", function() {
-  it("matches strings", function() {
-    var data = {
-      person: { jobDescription: "Nursing management and oversight" }
-    };
+  it("matches acceptable values", function() {
 
     var config = {
       "Anything to do with nursing is a good thing": {
         if: {
-          "person.jobDescription": { contains: "Nursing" }
+          "job": { contains: "Nursing" }
         },
         then: { "status.hasGoodJob": true }
       }
     };
-
-    var rules = new Rules(config);
-    var results = rules.run(data);
-
-    assert.equal(results.status.hasGoodJob, true);
+    [
+      {job: "I'm into Nursing"},
+      {job: "Nursing specialist"},
+      {job: ["medical", "Nursing"]},
+    ].forEach(function(data) {
+      var rules = new Rules(config);
+      var results = rules.run(data);
+      console.log(data);
+      assert.equal(results.status.hasGoodJob, true);
+    });
   });
 
-  it("matches arrays", function() {
-    var data = {
-      person: { friends: ["John", "Bob", "Susan"] }
-    };
+  it("accepts caseSensitive:false option", function() {
 
     var config = {
-      "People who know Bob": {
+      "Anything to do with nursing is a good thing": {
         if: {
-          "person.friends": { contains: "Bob" }
+          "job": { contains: "Nursing" }
         },
-        then: { "status.knowsBob": true }
-      },
-      "People who know Karen": {
-        if: {
-          "person.friends": { contains: "Karen" }
-        },
-        then: { "status.knowsKaren": true }
+        then: { "status.hasGoodJob": true }
       }
     };
+    [
+      {job: "I'm into nursing"},
+      {job: "nursing specialist"},
+      {job: ["medical", "nursing"]},
+    ].forEach(function(data) {
+      var rules = new Rules(config, {caseSensitive: false});
+      var results = rules.run(data);
+      console.log(data);
+      assert.equal(results.status.hasGoodJob, true);
+    });
+  });
 
-    var rules = new Rules(config);
-    var results = rules.run(data);
+  it("doesn't match bad values", function() {
 
-    assert(results.status.knowsBob);
-    assert(!results.status.knowsKaren);
+    var config = {
+      "Anything to do with nursing is a good thing": {
+        if: {
+          "job": { contains: "Nursing" }
+        },
+        then: { "status.hasGoodJob": true }
+      }
+    };
+    [
+      {job: "I'm into nursing"},
+      {job: "nursing specialist"},
+      {job: ["medical", "nursing"]},
+    ].forEach(function(data) {
+      var rules = new Rules(config);
+      var results = rules.run(data);
+      console.log(data);
+      assert.equal(results.status, undefined);
+    });
   });
 });
